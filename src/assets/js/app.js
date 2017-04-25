b01d462244f7a9a986b7b92632a0811236094937
Tymofii <timsc7@gmail.com>
2017-04-25 09:39:26 +0300

    24 april

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation();
+
+// $(document).ready(function () {
+//     $('.img-row2').slick({
+//         infinite: true,
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         autoplay: true
+//     });
+// });
+// $('head-block').css({"position":"relative", "top":"200px"});
